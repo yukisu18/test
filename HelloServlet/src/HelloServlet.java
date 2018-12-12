959678c350a920e83df7d12c9e53484b1450a854
yukisu18 <haru.isk@gmail.com>
2018-12-12 21:39:39 +0900

    20181212

--- HelloServlet/src/HelloServlet.java
+++ HelloServlet/src/HelloServlet.java
@@ -1,8 +1,12 @@
 
+import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/HelloServlet")
 public class HelloServlet extends HttpServlet {
@@ -12,8 +16,8 @@ public class HelloServlet extends HttpServlet {
 	}
 
 protected void doGet(HttpServletRequest request, HttpServletResponse response) throws
-ServletException, I0Exception{
-	PrintWriter out=response,getWriter();
+ServletException, IOException{
+	PrintWriter out=response.getWriter();
 	out.println("<HTML>");
 	out.println("<BODY>");
 	out.println("<H3>Hello Servlet!</H3>");
